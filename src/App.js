bba953677e0690cf72b0cd15e0908015204af8bb
Edith-Heidmann <eheidmann@gmail.com>
2022-04-11 13:37:14 +0200

    Initial environment setup commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
-function App() {
+// Views / Components
+import Home from "./Views/Home/Home";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <Router>
+        {/* // Todo: Scroll to top will go here */}
+        {/* // Todo: Nav will go here */}
 
+        <main>
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+
+            {/* Redirects all unknown routes entered to home page view - Replaces Redirect in react-router-dom 6+ */}
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </main>
+
+        {/* // Todo: Footer will go here.. */}
+      </Router>
+    </>
+  );
+};
 export default App;
