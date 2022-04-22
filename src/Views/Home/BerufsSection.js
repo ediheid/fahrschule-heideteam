1670f2f65cb3c2465a3b3b4ccc413caf33193d8c
Edith-Heidmann <eheidmann@gmail.com>
2022-04-22 17:40:13 +0200

    Finished Berufs section - added link and AOS

--- src/Views/Home/BerufsSection.js
+++ src/Views/Home/BerufsSection.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 import styles from "../Home/home.module.scss";
 
 import classroom from "../Home/Static/presentation.png";
-import truck from "../Home/Static/truck.png";
+
 import semi from "../Home/Static/semi.png";
 import certificate from "../Home/Static/certificate.png";
 import bus from "../Home/Static/bus.png";
 
+// AOS
+import Aos from "aos";
+import "aos/dist/aos.css";
+
 const BerufsSection = () => {
+  // AOS functionality
+  useEffect(() => {
+    Aos.init({ duration: 1000, once: false });
+  }, []);
+
   return (
     <section className={styles["berufs-section"]}>
       <h2
@@ -20,15 +30,12 @@ const BerufsSection = () => {
       </h2>
 
       <div className={styles["berufs-background-container"]}>
-        <div className={styles["berufs-icons-container"]}>
+        <div data-aos="fade-up" className={styles["berufs-icons-container"]}>
           <img
             src={classroom}
             alt="Teacher icons created by Nikita Golubev - Flaticon - https://www.flaticon.com/free-icons/teacher"
           ></img>
-          {/* <img
-            src={truck}
-            alt="Truck icons created by Freepik - Flaticon - https://www.flaticon.com/free-icons/truck"
-          ></img> */}
+
           <img
             src={semi}
             alt="Truck icons created by Freepik - Flaticon - https://www.flaticon.com/free-icons/truck"
@@ -44,7 +51,18 @@ const BerufsSection = () => {
           ></img>
         </div>
 
-        <button className={styles["berufs-button"]}>Mehr Information</button>
+        <p data-aos="fade-up" className={styles["berufs-text"]}>
+          Entsprechend des EU-Berufskraftfahrer-Qualifikationsgesetzes führen
+          wir die Weiterbildung von LKW- und Busfahrern durch
+        </p>
+
+        <Link
+          data-aos="fade-up"
+          to="/berufskraftfahrerqualifikation"
+          alt="Link to Berufskraftfahrerqualifikation page"
+        >
+          <button className={styles["berufs-button"]}>Mehr Information</button>
+        </Link>
       </div>
     </section>
   );
