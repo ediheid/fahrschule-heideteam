0743ffc63560521862eb7a289a63ddd0e4cf60e3
Edith-Heidmann <eheidmann@gmail.com>
2022-04-29 11:50:34 +0200

    Swapped Motion for React Reveal

--- src/Components/Navbar/MobileNav.js
+++ src/Components/Navbar/MobileNav.js
@@ -1,9 +1,9 @@
 import React, { useContext } from "react";
 
+import Roll from "react-reveal/Fade";
+
 import { Link } from "react-router-dom";
 import { AppContext } from "../../App";
-
-import { motion } from "framer-motion";
 
 import styles from "../Navbar/mobile-navigation.module.scss";
 
@@ -16,148 +16,145 @@ const MobileNav = () => {
   const MobileNavContext = useContext(AppContext);
 
   // Animation rules for Framer-motion on dropdown container
-  const animateFrom = { opacity: 0, y: -40 };
-  const animateTo = { opacity: 1, y: 0 };
 
   return (
     <>
       {/* // Todo: Look into more settings for animation */}
-      <motion.nav
-        initial={animateFrom}
-        animate={animateTo}
-        className={styles["dropdown-nav-container"]}
-      >
-        {/* <nav
+
+      <Roll top cascade duration={500}>
+        <nav className={styles["dropdown-nav-container"]}>
+          {/* <nav
         className={
           !MobileNavContext.isNavOpen
             ? `${styles["dropdown-nav-container"]}`
             : `${styles["dropdown-fix"]}`
         }
       > */}{" "}
-        <ul className={styles["nav-list"]}>
-          <hr />
+          <ul className={styles["nav-list"]}>
+            <hr />
 
-          {/* Note: For mobile Links <li/> lives inside <Link/> to make sure full item is active and redirects to selected page - see in stylesheet .nav-list - li */}
+            {/* Note: For mobile Links <li/> lives inside <Link/> to make sure full item is active and redirects to selected page - see in stylesheet .nav-list - li */}
 
-          {/* // ? Home */}
+            {/* // ? Home */}
 
-          <Link
-            onClick={MobileNavContext.isNavOpen}
-            className={styles["nav-links"]}
-            to="/"
-            alt="Link to landing page"
-          >
-            {" "}
-            <li>Home </li>
-          </Link>
+            <Link
+              onClick={MobileNavContext.isNavOpen}
+              className={styles["nav-links"]}
+              to="/"
+              alt="Link to landing page"
+            >
+              {" "}
+              <li>Home </li>
+            </Link>
 
-          <hr />
+            <hr />
 
-          {/* // ? Führerschein Classes */}
+            {/* // ? Führerschein Classes */}
 
-          <Link
-            onClick={MobileNavContext.isNavOpen}
-            className={styles["nav-links"]}
-            to="/klassen"
-            alt="Link to Führerschein Klassen page"
-          >
-            <li> Führerschein Klassen</li>
-          </Link>
+            <Link
+              onClick={MobileNavContext.isNavOpen}
+              className={styles["nav-links"]}
+              to="/klassen"
+              alt="Link to Führerschein Klassen page"
+            >
+              <li> Führerschein Klassen</li>
+            </Link>
 
-          <hr />
+            <hr />
 
-          {/* // ? Team */}
+            {/* // ? Team */}
 
-          <Link
-            onClick={MobileNavContext.isNavOpen}
-            className={styles["nav-links"]}
-            to="/team"
-            alt="Link to team page"
-          >
-            <li> Team</li>
-          </Link>
+            <Link
+              onClick={MobileNavContext.isNavOpen}
+              className={styles["nav-links"]}
+              to="/team"
+              alt="Link to team page"
+            >
+              <li> Team</li>
+            </Link>
 
-          <hr />
+            <hr />
 
-          {/* // ? Preise */}
+            {/* // ? Preise */}
 
-          <Link
-            onClick={MobileNavContext.isNavOpen}
-            className={styles["nav-links"]}
-            to="/preise"
-            alt="Link to preise page"
-          >
-            <li> Preise</li>
-          </Link>
+            <Link
+              onClick={MobileNavContext.isNavOpen}
+              className={styles["nav-links"]}
+              to="/preise"
+              alt="Link to preise page"
+            >
+              <li> Preise</li>
+            </Link>
 
-          <hr />
+            <hr />
 
-          {/* // ? Berufskraftfahrerqualifikation */}
+            {/* // ? Berufskraftfahrerqualifikation */}
 
-          <Link
-            onClick={MobileNavContext.isNavOpen}
-            className={styles["nav-links"]}
-            to="/berufskraftfahrerqualifikation"
-            alt="Link to Berufskraftfahrerqualifikation page"
-          >
-            <li> Berufskraftfahrerqualifikation</li>
-          </Link>
+            <Link
+              onClick={MobileNavContext.isNavOpen}
+              className={styles["nav-links"]}
+              to="/berufskraftfahrerqualifikation"
+              alt="Link to Berufskraftfahrerqualifikation page"
+            >
+              <li> Berufskraftfahrerqualifikation</li>
+            </Link>
 
-          <hr />
+            <hr />
 
-          {/* // ? News */}
+            {/* // ? News */}
 
-          <Link
-            onClick={MobileNavContext.isNavOpen}
-            className={styles["nav-links"]}
-            to="/news"
-            alt="Link to news page"
-          >
-            <li> News</li>
-          </Link>
+            <Link
+              onClick={MobileNavContext.isNavOpen}
+              className={styles["nav-links"]}
+              to="/news"
+              alt="Link to news page"
+            >
+              <li> News</li>
+            </Link>
 
-          <hr />
+            <hr />
 
-          {/* // ? Contact */}
+            {/* // ? Contact */}
 
-          <Link
-            onClick={MobileNavContext.isNavOpen}
-            className={styles["nav-links"]}
-            to="/kontakt"
-            alt="Link to contact page"
-          >
-            <li> Kontakt</li>
-          </Link>
+            <Link
+              onClick={MobileNavContext.isNavOpen}
+              className={styles["nav-links"]}
+              to="/kontakt"
+              alt="Link to contact page"
+            >
+              <li> Kontakt</li>
+            </Link>
 
-          <hr />
+            <hr />
 
-          {/* // ! To remove when no longer needed */}
-          {/* // Todo: Add Hashlink */}
-          {/* // ! Currently links to section on News page */}
-          {/* // ? Covid Info page */}
+            {/* // ! To remove when no longer needed */}
+            {/* // Todo: Add Hashlink */}
+            {/* // ! Currently links to section on News page */}
+            {/* // ? Covid Info page */}
 
-          <Link
-            onClick={MobileNavContext.isNavOpen}
-            className={styles["nav-links"]}
-            to="/news"
-            alt="Link to covid infopage"
-          >
-            <li className={`${styles["covid-container"]} `}>
-              <BsInfoCircle
-                className={`${styles["nav-links"]} ${styles["info-icon"]}`}
-              />{" "}
-              <span
-                className={`${styles["covid-span"]} ${styles["nav-links"]} `}
-              >
-                {" "}
-                Covid Info
-              </span>
-            </li>
-          </Link>
+            <Link
+              onClick={MobileNavContext.isNavOpen}
+              className={styles["nav-links"]}
+              to="/news"
+              alt="Link to covid infopage"
+            >
+              <li className={`${styles["covid-container"]} `}>
+                <BsInfoCircle
+                  className={`${styles["nav-links"]} ${styles["info-icon"]}`}
+                />{" "}
+                <span
+                  className={`${styles["covid-span"]} ${styles["nav-links"]} `}
+                >
+                  {" "}
+                  Covid Info
+                </span>
+              </li>
+            </Link>
 
-          <hr />
-        </ul>
-      </motion.nav>
+            <hr />
+          </ul>
+        </nav>
+      </Roll>
     </>
   );
 };
