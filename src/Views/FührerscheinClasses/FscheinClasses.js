359d6eb5914beb80ae5a2e6d90080e1137dce9c6
Edith-Heidmann <eheidmann@gmail.com>
2022-05-04 18:51:52 +0200

    Added first klasse item

--- src/Views/FührerscheinClasses/FscheinClasses.js
+++ src/Views/FührerscheinClasses/FscheinClasses.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { HashLink } from "react-router-hash-link";
 
+import SubMenu from "./SubMenu";
+
 import styles from "../FührerscheinClasses/fschein-classes.module.scss";
 
 // Card Components
@@ -12,61 +14,89 @@ import BusCard from "./BusCard";
 import TraktorCard from "./TraktorCard";
 
 // Icons
+import A from "../FührerscheinClasses/Static/icon-a-v3.png";
 
 // Every second card has the image to the right
 const FscheinClasses = () => {
   return (
     <div className={styles["classes-page-container"]}>
-      <div className={styles["classes-sub-menu"]}>
-        {/* // Todo: Hashlinks ready to setup to sections once implemented */}
-        <ul>
-          <HashLink to="/" className={styles["hashlink"]}>
-            <li>A</li>
-          </HashLink>
-          <HashLink to="/" className={styles["hashlink"]}>
-            {" "}
-            <li>A1</li>
-          </HashLink>
-          <HashLink to="/" className={styles["hashlink"]}>
-            <li>A2</li>
-          </HashLink>
-          <HashLink to="/" className={styles["hashlink"]}>
-            {" "}
-            <li>AM</li>
-          </HashLink>
-          <HashLink to="/" className={styles["hashlink"]}>
-            {" "}
-            <li>B / B197</li>
-          </HashLink>
-          <HashLink to="/" className={styles["hashlink"]}>
-            <li>BE</li>
-          </HashLink>
-          <HashLink to="/" className={styles["hashlink"]}>
-            <li>C</li>
-          </HashLink>
-          <HashLink to="/" className={styles["hashlink"]}>
-            {" "}
-            <li>C1</li>
-          </HashLink>
-          <HashLink to="/" className={styles["hashlink"]}>
-            {" "}
-            <li>C1E</li>
-          </HashLink>
-          <HashLink to="/" className={styles["hashlink"]}>
-            <li>CE</li>
-          </HashLink>
-          <HashLink to="/" className={styles["hashlink"]}>
-            <li>D</li>
-          </HashLink>
-          <HashLink to="/" className={styles["hashlink"]}>
-            <li>D1</li>
-          </HashLink>
-          <HashLink to="/" className={styles["hashlink"]}>
-            <li>T</li>
-          </HashLink>
-        </ul>
-      </div>
+      {/* // Submenu with hashlinks to class sections */}
+      <SubMenu />
 
+      <section className={styles["class-section"]}>
+        <div className={styles["class-heading-circle"]}>
+          <img
+            className={styles["class-icons"]}
+            src={A}
+            alt="Motorcycle icons created by Freepik - Flaticon - https://www.flaticon.com/free-icons/motorcycle"
+          ></img>
+          <h2 className={styles["headings"]}>Klasse A</h2>
+        </div>
+
+        <div className={styles["section-content-container"]}>
+          <div className={styles["text-container"]}>
+            <h4> Fahrzeugart Schwere Krafträder</h4>
+            <ul>
+              <li>
+                <b>„Schwere“ Krafträder </b> (Zweiräder, auch mit Beiwagen) mit
+                einem Hubraum von mehr als 50 cm3 und einer bauartbedingten
+                Höchstgeschwindigkeit von mehr als 45 km/h
+              </li>
+              <li>
+                <b>Dreirädrige Kraftfahrzeuge</b> mit einer Motorleistung von
+                mehr als 15 kW Mindestalter: <b>24</b> Jahre
+              </li>
+              <li>
+                <b>20</b> Jahre nach mindestens zweijährigem Vorbesitz der
+                Klasse A2
+              </li>
+              <li>
+                <b>21</b> Jahre für dreirädrige Kraftfahrzeuge der Klasse A
+              </li>
+              <li>
+                Geltungsdauer: <b>ohne Befristung</b>
+              </li>
+              <li>
+                Vorbesitz erforderlich: <b>NEIN</b>
+              </li>
+              <li>
+                Beinhaltet Klasse: <b>A2, A1, AM</b>
+              </li>
+              <li>
+                Sehvermögen: <b>Sehtest</b>
+              </li>
+              <li>
+                Erste Hilfe: <b>Erste-Hilfe-Kurs</b>
+              </li>
+              <li>
+                <b>
+                  Bei uns lernen Motorradfahrer von erfahrenen Motorradfahrern.
+                </b>
+              </li>
+            </ul>
+
+            <div className={styles["price-section"]}>
+              <h3>Preisliste</h3>
+              <ul>
+                <li>Grundbetrag: 350 €</li>
+                <li>Übungsfahrt: 60 €</li>
+                <li>
+                  Sonderfahrten (mind. 5X Überlandfahrt 4x autobahnfahrt
+                  3xNachtfahrt) pro 45 Minuten 70 €
+                </li>
+
+                <br />
+                <li>
+                  <b>Vorstellen zu Theoretischen Prüfung 50 €</b>
+                </li>
+                <li>
+                  <b>Vorstellen zur praktischen Prüfung 160 €</b>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
       {/* // ? PKW */}
 
       {/* <PkwCard /> */}
---
+++ src/Views/FührerscheinClasses/SubMenu.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { HashLink } from "react-router-hash-link";
+
+import styles from "../FührerscheinClasses/fschein-classes.module.scss";
+
+const SubMenu = () => {
+  return (
+    <div className={styles["classes-sub-menu"]}>
+      {/* // Todo: Hashlinks ready to setup to sections once implemented */}
+      <ul>
+        <HashLink to="/" className={styles["hashlink"]}>
+          <li>A</li>
+        </HashLink>
+        <HashLink to="/" className={styles["hashlink"]}>
+          {" "}
+          <li>A1</li>
+        </HashLink>
+        <HashLink to="/" className={styles["hashlink"]}>
+          <li>A2</li>
+        </HashLink>
+        <HashLink to="/" className={styles["hashlink"]}>
+          {" "}
+          <li>AM</li>
+        </HashLink>
+        <HashLink to="/" className={styles["hashlink"]}>
+          {" "}
+          <li>B / B197</li>
+        </HashLink>
+        <HashLink to="/" className={styles["hashlink"]}>
+          <li>BE</li>
+        </HashLink>
+        <HashLink to="/" className={styles["hashlink"]}>
+          <li>C</li>
+        </HashLink>
+        <HashLink to="/" className={styles["hashlink"]}>
+          {" "}
+          <li>C1</li>
+        </HashLink>
+        <HashLink to="/" className={styles["hashlink"]}>
+          {" "}
+          <li>C1E</li>
+        </HashLink>
+        <HashLink to="/" className={styles["hashlink"]}>
+          <li>CE</li>
+        </HashLink>
+        <HashLink to="/" className={styles["hashlink"]}>
+          <li>D</li>
+        </HashLink>
+        <HashLink to="/" className={styles["hashlink"]}>
+          <li>D1</li>
+        </HashLink>
+        <HashLink to="/" className={styles["hashlink"]}>
+          <li>T</li>
+        </HashLink>
+      </ul>
+    </div>
+  );
+};
+
+export default SubMenu;
