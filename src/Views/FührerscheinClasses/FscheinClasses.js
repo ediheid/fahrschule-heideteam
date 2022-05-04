fe067298e8227a1af13a33243856ce584b5a3849
Edith-Heidmann <eheidmann@gmail.com>
2022-05-04 17:20:19 +0200

    Setup hashlinks and hover effects

--- src/Views/FührerscheinClasses/FscheinClasses.js
+++ src/Views/FührerscheinClasses/FscheinClasses.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { HashLink } from "react-router-hash-link";
 
 import styles from "../FührerscheinClasses/fschein-classes.module.scss";
 
@@ -10,11 +11,61 @@ import LkwCard from "./LkwCard";
 import BusCard from "./BusCard";
 import TraktorCard from "./TraktorCard";
 
+// Icons
+
 // Every second card has the image to the right
 const FscheinClasses = () => {
   return (
     <div className={styles["classes-page-container"]}>
-      <div className={styles["classes-sub-menu"]}></div>
+      <div className={styles["classes-sub-menu"]}>
+        {/* // Todo: Hashlinks ready to setup to sections once implemented */}
+        <ul>
+          <HashLink to="/" className={styles["hashlink"]}>
+            <li>A</li>
+          </HashLink>
+          <HashLink to="/" className={styles["hashlink"]}>
+            {" "}
+            <li>A1</li>
+          </HashLink>
+          <HashLink to="/" className={styles["hashlink"]}>
+            <li>A2</li>
+          </HashLink>
+          <HashLink to="/" className={styles["hashlink"]}>
+            {" "}
+            <li>AM</li>
+          </HashLink>
+          <HashLink to="/" className={styles["hashlink"]}>
+            {" "}
+            <li>B / B197</li>
+          </HashLink>
+          <HashLink to="/" className={styles["hashlink"]}>
+            <li>BE</li>
+          </HashLink>
+          <HashLink to="/" className={styles["hashlink"]}>
+            <li>C</li>
+          </HashLink>
+          <HashLink to="/" className={styles["hashlink"]}>
+            {" "}
+            <li>C1</li>
+          </HashLink>
+          <HashLink to="/" className={styles["hashlink"]}>
+            {" "}
+            <li>C1E</li>
+          </HashLink>
+          <HashLink to="/" className={styles["hashlink"]}>
+            <li>CE</li>
+          </HashLink>
+          <HashLink to="/" className={styles["hashlink"]}>
+            <li>D</li>
+          </HashLink>
+          <HashLink to="/" className={styles["hashlink"]}>
+            <li>D1</li>
+          </HashLink>
+          <HashLink to="/" className={styles["hashlink"]}>
+            <li>T</li>
+          </HashLink>
+        </ul>
+      </div>
 
       {/* // ? PKW */}
 
