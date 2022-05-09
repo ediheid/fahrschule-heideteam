487cd5e95de4d4525b0346905ac772b68461a5b5
Edith-Heidmann <eheidmann@gmail.com>
2022-05-09 15:26:07 +0200

    Modified styles

--- src/Views/FührerscheinClasses/Slideshow/Gallery.js
+++ src/Views/FührerscheinClasses/Slideshow/Gallery.js
@@ -36,19 +36,20 @@ const Gallery = () => {
 
   const customStyles = {
     content: {
-      // top: "50%",
-      // left: "50%",
-      // right: "auto",
-      // bottom: "auto",
-      // marginRight: "-50%",
-      // transform: "translate(-50%, -50%)",
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
 
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
+      // display: "flex",
+      // justifyContent: "center",
+      // alignItems: "center",
 
       height: "50vh",
-      // backgroundColor: "red",
+      backgroundColor: "white",
+      border: "none",
     },
   };
 
