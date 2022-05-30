22ed0435f57274e73e1cbd5ab00b3a886956ccfd
Edith-Heidmann <eheidmann@gmail.com>
2022-05-30 20:08:57 +0200

    Added news info

--- src/Views/News/News.js
+++ src/Views/News/News.js
@@ -3,7 +3,31 @@ import React from "react";
 const News = () => {
   return (
     <>
-      <div>News page will go here</div>
+      <div>
+        Aufgrund verschiedenster Hintergründe ist es im Augenblick nicht
+        möglich, kurzfristig in eine Führerscheinausbildung (vorrangig Klasse B
+        und alle A-Klassen) zu starten.
+      </div>
+      {/* // Todo: instead of putting the email here, put a hashlink to the contact
+      form */}
+      <div>
+        Bitte informieren Sie sich persönlich über unser Büro (jeweils mittwochs
+        von 15.00 - 18.00 Uhr) oder jederzeit per Mail unter
+        info@fsheideteam.de.
+      </div>
+      <div>
+        Aus organisatorischen Gründen können wir zum jetzigen Zeitpunkt die
+        theoretische Ausbildung (Grundstoff) nur in den Ferien - als
+        Feriencrashkurs - anbieten.
+      </div>
+      <div>
+        Bitte bedenken Sie auch, dass es im Moment zu sehr langen Wartezeiten
+        bei den Behörden (Fahrerlaubnisbehörde) und auch bei Prüfungsterminen
+        für Theorie und Praxis (DEKRA) kommt.
+      </div>
+
+      {/* // ! Is this actual text? */}
+      {/* <div>Zum Abklären weiterer Fragen sprechen Sie uns einfach an!</div> */}
       <h3>Covid</h3>
       <p>Keine aktuellen Einschränkungen</p>
     </>
