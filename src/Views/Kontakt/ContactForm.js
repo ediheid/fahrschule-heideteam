aab65051aae123e212091b741694f0b2167d7a09
Edith-Heidmann <eheidmann@gmail.com>
2022-05-31 14:55:43 +0200

    Added more to contact form

--- src/Views/Kontakt/ContactForm.js
+++ src/Views/Kontakt/ContactForm.js
@@ -54,55 +54,83 @@ const ContactForm = () => {
   };
 
   return (
-    <div className={styles["contact-form-container"]} id="contact-form">
-      <form className={styles["contact-form"]}>
-        <input
-          className={styles["contact-form-input"]}
-          type="text"
-          placeholder="Vorname"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+    <section className={styles["contact-form-section"]}>
+      <div className={styles["contact-form-container"]} id="contact-form">
+        <h3 className={styles["form-heading"]}>
+          Schreib <b>uns</b>
+        </h3>
 
-        <input
-          className={styles["contact-form-input"]}
-          type="text"
-          placeholder="Nachname"
-          value={lastname}
-          onChange={(e) => setLastname(e.target.value)}
-        />
+        <div className={styles["heading-underline"]}></div>
 
-        <input
-          className={styles["contact-form-input"]}
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+        <form className={styles["contact-form"]}>
+          <label className={styles["contact-form-label"]} htmlFor="fistname">
+            Vorname
+          </label>
+          <input
+            className={styles["contact-form-input"]}
+            type="text"
+            placeholder="Vorname"
+            value={name}
+            name="fistname"
+            onChange={(e) => setName(e.target.value)}
+          />
 
-        <input
-          className={styles["contact-form-input"]}
-          type="text"
-          placeholder="Thema"
-          value={subject}
-          onChange={(e) => setSubject(e.target.value)}
-        />
+          <label className={styles["contact-form-label"]} htmlFor="lastname">
+            Nachname
+          </label>
+          <input
+            className={styles["contact-form-input"]}
+            type="text"
+            placeholder="Nachname"
+            value={lastname}
+            name="lastname"
+            onChange={(e) => setLastname(e.target.value)}
+          />
 
-        <textarea
-          className={styles["contact-form-input"]}
-          type="text"
-          placeholder="Schreiben Sie ihre Nachricht hier"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        ></textarea>
-        <button type="submit" onClick={submit}>
-          Nachricht senden
-        </button>
-        {/* <span className={emailSent ? "visible" : null}>
+          <label className={styles["contact-form-label"]} htmlFor="email">
+            Email
+          </label>
+          <input
+            className={styles["contact-form-input"]}
+            type="email"
+            placeholder="Email"
+            value={email}
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <label className={styles["contact-form-label"]} htmlFor="subject">
+            Subject
+          </label>
+          <input
+            className={styles["contact-form-input"]}
+            type="text"
+            placeholder="Thema"
+            value={subject}
+            name="subject"
+            onChange={(e) => setSubject(e.target.value)}
+          />
+
+          <label className={styles["contact-form-label"]} htmlFor="message">
+            Nachricht
+          </label>
+          <textarea
+            className={`${styles["contact-form-input"]} ${styles["text-container"]}`}
+            type="text"
+            placeholder="Schreiben Sie ihre Nachricht hier"
+            value={message}
+            name="message"
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
+          <button type="submit" onClick={submit}>
+            Nachricht senden
+          </button>
+          {/* <span className={emailSent ? "visible" : null}>
         Thank you for your message, we will be in touch in no time!
       </span> */}
-      </form>
-    </div>
+        </form>
+      </div>
+    </section>
   );
 };
 
