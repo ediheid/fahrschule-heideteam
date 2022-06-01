69e487f3676f627b36b1b81026733685240f93e5
Edith-Heidmann <eheidmann@gmail.com>
2022-06-01 20:01:56 +0200

    Added dsgvo

--- src/Views/DSGVO/Datenschutz.js
+++ src/Views/DSGVO/Datenschutz.js
@@ -130,6 +130,154 @@ const Datenschutz = () => {
           Device-Fingerprinting) im Sinne des TTDSG umfasst. Die Einwilligung
           ist jederzeit widerrufbar.
         </p>
+
+        <p className={styles["paragraphs"]}>
+          <b>Auftragsverarbeitung</b>
+        </p>
+
+        <p className={styles["paragraphs"]}>
+          Wir haben einen Vertrag über Auftragsverarbeitung (AVV) mit dem oben
+          genannten Anbieter geschlossen. Hierbei handelt es sich um einen
+          datenschutzrechtlich vorgeschriebenen Vertrag, der gewährleistet, dass
+          dieser die personenbezogenen Daten unserer Websitebesucher nur nach
+          unseren Weisungen und unter Einhaltung der DSGVO verarbeitet.
+        </p>
+
+        <h4 className={styles["sub-headings"]}>
+          3. Allgemeine Hinweise und Pflichtinformationen
+        </h4>
+
+        <h5 className={styles["secondary-sub-headings"]}>Datenschutz</h5>
+
+        <p className={styles["paragraphs"]}>
+          Die Betreiber dieser Seiten nehmen den Schutz Ihrer persönlichen Daten
+          sehr ernst. Wir behandeln Ihre personenbezogenen Daten vertraulich und
+          entsprechend den gesetzlichen Datenschutzvorschriften sowie dieser
+          Datenschutzerklärung.
+        </p>
+
+        <p className={styles["paragraphs"]}>
+          Wenn Sie diese Website benutzen, werden verschiedene personenbezogene
+          Daten erhoben. Personenbezogene Daten sind Daten, mit denen Sie
+          persönlich identifiziert werden können. Die vorliegende
+          Datenschutzerklärung erläutert, welche Daten wir erheben und wofür wir
+          sie nutzen. Sie erläutert auch, wie und zu welchem Zweck das
+          geschieht.
+        </p>
+
+        <p className={styles["paragraphs"]}>
+          Wir weisen darauf hin, dass die Datenübertragung im Internet (z. B.
+          bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann.
+          Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist
+          nicht möglich.
+        </p>
+
+        <h5 className={styles["secondary-sub-headings"]}>
+          Hinweis zur verantwortlichen Stelle
+        </h5>
+
+        <p className={styles["paragraphs"]}>
+          Die verantwortliche Stelle für die Datenverarbeitung auf dieser
+          Website ist:
+        </p>
+
+        <p className={styles["paragraphs"]}>
+          Heideteam Fahrschul GmbH Pillnitzer Str. 8 - 01454 Radeberg
+        </p>
+
+        <span className={styles["spans"]}>
+          Telefon:{" "}
+          <a
+            className={styles["anchor-tags"]}
+            href="tel:+49-3528-226-9934"
+            alt="Link to call"
+          >
+            {" "}
+            +49 3528 226 9934{" "}
+          </a>
+        </span>
+        <span className={styles["spans"]}>
+          E-Mail{" "}
+          <a
+            className={styles["anchor-tags"]}
+            href="mailto:info@fsheideteam.de"
+            alt="Link to call"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            info@fsheideteam.de{" "}
+          </a>
+        </span>
+
+        <p className={styles["paragraphs"]}>
+          Verantwortliche Stelle ist die natürliche oder juristische Person, die
+          allein oder gemeinsam mit anderen über die Zwecke und Mittel der
+          Verarbeitung von personenbezogenen Daten (z. B. Namen, E-Mail-Adressen
+          o. Ä.) entscheidet.
+        </p>
+
+        <h5 className={styles["secondary-sub-headings"]}>Speicherdauer</h5>
+
+        <p className={styles["paragraphs"]}>
+          Soweit innerhalb dieser Datenschutzerklärung keine speziellere
+          Speicherdauer genannt wurde, verbleiben Ihre personenbezogenen Daten
+          bei uns, bis der Zweck für die Datenverarbeitung entfällt. Wenn Sie
+          ein berechtigtes Löschersuchen geltend machen oder eine Einwilligung
+          zur Datenverarbeitung widerrufen, werden Ihre Daten gelöscht, sofern
+          wir keine anderen rechtlich zulässigen Gründe für die Speicherung
+          Ihrer personenbezogenen Daten haben (z. B. steuer- oder
+          handelsrechtliche Aufbewahrungsfristen); im letztgenannten Fall
+          erfolgt die Löschung nach Fortfall dieser Gründe.
+        </p>
+
+        <h5 className={styles["secondary-sub-headings"]}>
+          Allgemeine Hinweise zu den Rechtsgrundlagen der Datenverarbeitung auf
+          dieser Website
+        </h5>
+
+        <p className={styles["paragraphs"]}>
+          Sofern Sie in die Datenverarbeitung eingewilligt haben, verarbeiten
+          wir Ihre personenbezogenen Daten auf Grundlage von Art. 6 Abs. 1 lit.
+          a DSGVO bzw. Art. 9 Abs. 2 lit. a DSGVO, sofern besondere
+          Datenkategorien nach Art. 9 Abs. 1 DSGVO verarbeitet werden. Im Falle
+          einer ausdrücklichen Einwilligung in die Übertragung personenbezogener
+          Daten in Drittstaaten erfolgt die Datenverarbeitung außerdem auf
+          Grundlage von Art. 49 Abs. 1 lit. a DSGVO. Sofern Sie in die
+          Speicherung von Cookies oder in den Zugriff auf Informationen in Ihr
+          Endgerät (z. B. via Device-Fingerprinting) eingewilligt haben, erfolgt
+          die Datenverarbeitung zusätzlich auf Grundlage von § 25 Abs. 1 TTDSG.
+          Die Einwilligung ist jederzeit widerrufbar. Sind Ihre Daten zur
+          Vertragserfüllung oder zur Durchführung vorvertraglicher Maßnahmen
+          erforderlich, verarbeiten wir Ihre Daten auf Grundlage des Art. 6 Abs.
+          1 lit. b DSGVO. Des Weiteren verarbeiten wir Ihre Daten, sofern diese
+          zur Erfüllung einer rechtlichen Verpflichtung erforderlich sind auf
+          Grundlage von Art. 6 Abs. 1 lit. c DSGVO. Die Datenverarbeitung kann
+          ferner auf Grundlage unseres berechtigten Interesses nach Art. 6 Abs.
+          1 lit. f DSGVO erfolgen. Über die jeweils im Einzelfall einschlägigen
+          Rechtsgrundlagen wird in den folgenden Absätzen dieser
+          Datenschutzerklärung informiert.
+        </p>
+
+        <h5 className={styles["secondary-sub-headings"]}>
+          Hinweis zur Datenweitergabe in die USA und sonstige Drittstaaten
+        </h5>
+
+        <p className={styles["paragraphs"]}>
+          Wir verwenden unter anderem Tools von Unternehmen mit Sitz in den USA
+          oder sonstigen datenschutzrechtlich nicht sicheren Drittstaaten. Wenn
+          diese Tools aktiv sind, können Ihre personenbezogene Daten in diese
+          Drittstaaten übertragen und dort verarbeitet werden. Wir weisen darauf
+          hin, dass in diesen Ländern kein mit der EU vergleichbares
+          Datenschutzniveau garantiert werden kann. Beispielsweise sind
+          US-Unternehmen dazu verpflichtet, personenbezogene Daten an
+          Sicherheitsbehörden herauszugeben, ohne dass Sie als Betroffener
+          hiergegen gerichtlich vorgehen könnten. Es kann daher nicht
+          ausgeschlossen werden, dass US-Behörden (z. B. Geheimdienste) Ihre auf
+          US-Servern befindlichen Daten zu Überwachungszwecken verarbeiten,
+          auswerten und dauerhaft speichern. Wir haben auf diese
+          Verarbeitungstätigkeiten keinen Einfluss.
+        </p>
       </div>
     </div>
   );
