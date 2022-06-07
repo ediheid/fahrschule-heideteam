5a4cf7fcbdcccc797ee1b937732fe48c89dc81c0
Edith-Heidmann <eheidmann@gmail.com>
2022-06-07 12:55:01 +0200

    Added DSGVO

--- src/Views/DSGVO/Datenschutz.js
+++ src/Views/DSGVO/Datenschutz.js
@@ -278,6 +278,322 @@ const Datenschutz = () => {
           auswerten und dauerhaft speichern. Wir haben auf diese
           Verarbeitungstätigkeiten keinen Einfluss.
         </p>
+
+        <h5 className={styles["secondary-sub-headings"]}>
+          Widerruf Ihrer Einwilligung zur Datenverarbeitung
+        </h5>
+
+        <p className={styles["paragraphs"]}>
+          Viele Datenverarbeitungsvorgänge sind nur mit Ihrer ausdrücklichen
+          Einwilligung möglich. Sie können eine bereits erteilte Einwilligung
+          jederzeit widerrufen. Die Rechtmäßigkeit der bis zum Widerruf
+          erfolgten Datenverarbeitung bleibt vom Widerruf unberührt.
+        </p>
+
+        <h5 className={styles["secondary-sub-headings"]}>
+          Widerspruchsrecht gegen die Datenerhebung in besonderen Fällen sowie
+          gegen Direktwerbung (Art. 21 DSGVO)
+        </h5>
+
+        <p className={styles["paragraphs"]}>
+          WENN DIE DATENVERARBEITUNG AUF GRUNDLAGE VON ART. 6 ABS. 1 LIT. E ODER
+          F DSGVO ERFOLGT, HABEN SIE JEDERZEIT DAS RECHT, AUS GRÜNDEN, DIE SICH
+          AUS IHRER BESONDEREN SITUATION ERGEBEN, GEGEN DIE VERARBEITUNG IHRER
+          PERSONENBEZOGENEN DATEN WIDERSPRUCH EINZULEGEN; DIES GILT AUCH FÜR EIN
+          AUF DIESE BESTIMMUNGEN GESTÜTZTES PROFILING. DIE JEWEILIGE
+          RECHTSGRUNDLAGE, AUF DENEN EINE VERARBEITUNG BERUHT, ENTNEHMEN SIE
+          DIESER DATENSCHUTZERKLÄRUNG. WENN SIE WIDERSPRUCH EINLEGEN, WERDEN WIR
+          IHRE BETROFFENEN PERSONENBEZOGENEN DATEN NICHT MEHR VERARBEITEN, ES
+          SEI DENN, WIR KÖNNEN ZWINGENDE SCHUTZWÜRDIGE GRÜNDE FÜR DIE
+          VERARBEITUNG NACHWEISEN, DIE IHRE INTERESSEN, RECHTE UND FREIHEITEN
+          ÜBERWIEGEN ODER DIE VERARBEITUNG DIENT DER GELTENDMACHUNG, AUSÜBUNG
+          ODER VERTEIDIGUNG VON RECHTSANSPRÜCHEN (WIDERSPRUCH NACH ART. 21 ABS.
+          1 DSGVO).
+        </p>
+
+        <p className={styles["paragraphs"]}>
+          WERDEN IHRE PERSONENBEZOGENEN DATEN VERARBEITET, UM DIREKTWERBUNG ZU
+          BETREIBEN, SO HABEN SIE DAS RECHT, JEDERZEIT WIDERSPRUCH GEGEN DIE
+          VERARBEITUNG SIE BETREFFENDER PERSONENBEZOGENER DATEN ZUM ZWECKE
+          DERARTIGER WERBUNG EINZULEGEN; DIES GILT AUCH FÜR DAS PROFILING,
+          SOWEIT ES MIT SOLCHER DIREKTWERBUNG IN VERBINDUNG STEHT. WENN SIE
+          WIDERSPRECHEN, WERDEN IHRE PERSONENBEZOGENEN DATEN ANSCHLIESSEND NICHT
+          MEHR ZUM ZWECKE DER DIREKTWERBUNG VERWENDET (WIDERSPRUCH NACH ART. 21
+          ABS. 2 DSGVO).
+        </p>
+
+        <h5 className={styles["secondary-sub-headings"]}>
+          Beschwerderecht bei der zuständigen Aufsichtsbehörde
+        </h5>
+
+        <p className={styles["paragraphs"]}>
+          Im Falle von Verstößen gegen die DSGVO steht den Betroffenen ein
+          Beschwerderecht bei einer Aufsichtsbehörde, insbesondere in dem
+          Mitgliedstaat ihres gewöhnlichen Aufenthalts, ihres Arbeitsplatzes
+          oder des Orts des mutmaßlichen Verstoßes zu. Das Beschwerderecht
+          besteht unbeschadet anderweitiger verwaltungsrechtlicher oder
+          gerichtlicher Rechtsbehelfe.
+        </p>
+
+        <h5 className={styles["secondary-sub-headings"]}>
+          Recht auf Datenübertragbarkeit
+        </h5>
+
+        <p className={styles["paragraphs"]}>
+          Sie haben das Recht, Daten, die wir auf Grundlage Ihrer Einwilligung
+          oder in Erfüllung eines Vertrags automatisiert verarbeiten, an sich
+          oder an einen Dritten in einem gängigen, maschinenlesbaren Format
+          aushändigen zu lassen. Sofern Sie die direkte Übertragung der Daten an
+          einen anderen Verantwortlichen verlangen, erfolgt dies nur, soweit es
+          technisch machbar ist.
+        </p>
+
+        <h5 className={styles["secondary-sub-headings"]}>
+          SSL- bzw. TLS-Verschlüsselung
+        </h5>
+
+        <p className={styles["paragraphs"]}>
+          Diese Seite nutzt aus Sicherheitsgründen und zum Schutz der
+          Übertragung vertraulicher Inhalte, wie zum Beispiel Bestellungen oder
+          Anfragen, die Sie an uns als Seitenbetreiber senden, eine SSL- bzw.
+          TLS- Verschlüsselung. Eine verschlüsselte Verbindung erkennen Sie
+          daran, dass die Adresszeile des Browsers von „http://“ auf „https://“
+          wechselt und an dem Schloss-Symbol in Ihrer Browserzeile.
+        </p>
+
+        <p className={styles["paragraphs"]}>
+          Wenn die SSL- bzw. TLS-Verschlüsselung aktiviert ist, können die
+          Daten, die Sie an uns übermitteln, nicht von Dritten mitgelesen
+          werden.
+        </p>
+
+        <h5 className={styles["secondary-sub-headings"]}>
+          Auskunft, Löschung und Berichtigung
+        </h5>
+
+        <p className={styles["paragraphs"]}>
+          Sie haben im Rahmen der geltenden gesetzlichen Bestimmungen jederzeit
+          das Recht auf unentgeltliche Auskunft über Ihre gespeicherten
+          personenbezogenen Daten, deren Herkunft und Empfänger und den Zweck
+          der Datenverarbeitung und ggf. ein Recht auf Berichtigung oder
+          Löschung dieser Daten. Hierzu sowie zu weiteren Fragen zum Thema
+          personenbezogene Daten können Sie sich jederzeit an uns wenden.
+        </p>
+
+        <h5 className={styles["secondary-sub-headings"]}>
+          Recht auf Einschränkung der Verarbeitung
+        </h5>
+
+        <p className={styles["paragraphs"]}>
+          Sie haben das Recht, die Einschränkung der Verarbeitung Ihrer
+          personenbezogenen Daten zu verlangen.
+        </p>
+
+        <p className={styles["paragraphs"]}>
+          Hierzu können Sie sich jederzeit an uns wenden. Das Recht auf
+          Einschränkung der Verarbeitung besteht in folgenden Fällen:
+        </p>
+
+        <p className={styles["paragraphs"]}>
+          <ul className={styles["list"]}>
+            <li>
+              Wenn Sie die Richtigkeit Ihrer bei uns gespeicherten
+              personenbezogenen Daten bestreiten, benötigen wir in der Regel
+              Zeit, um dies zu überprüfen. Für die Dauer der Prüfung haben Sie
+              das Recht, die Einschränkung der Verarbeitung Ihrer
+              personenbezogenen Daten zu verlangen.
+            </li>
+            <li>
+              Wenn die Verarbeitung Ihrer personenbezogenen Daten unrechtmäßig
+              geschah/geschieht, können Sie statt der Löschung die Einschränkung
+              der Datenverarbeitung verlangen.
+            </li>
+            <li>
+              Wenn wir Ihre personenbezogenen Daten nicht mehr benötigen, Sie
+              sie jedoch zur Ausübung, Verteidigung oder Geltendmachung von
+              Rechtsansprüchen benötigen, haben Sie das Recht, statt der
+              Löschung die Einschränkung der Verarbeitung Ihrer
+              personenbezogenen Daten zu verlangen.
+            </li>
+            <li>
+              Wenn Sie einen Widerspruch nach Art. 21 Abs. 1 DSGVO eingelegt
+              haben, muss eine Abwägung zwischen Ihren und unseren Interessen
+              vorgenommen werden. Solange noch nicht feststeht, wessen
+              Interessen überwiegen, haben Sie das Recht, die Einschränkung der
+              Verarbeitung Ihrer personenbezogenen Daten zu verlangen.
+            </li>
+          </ul>
+        </p>
+
+        <p className={styles["paragraphs"]}>
+          Wenn Sie die Verarbeitung Ihrer personenbezogenen Daten eingeschränkt
+          haben, dürfen diese Daten – von ihrer Speicherung abgesehen – nur mit
+          Ihrer Einwilligung oder zur Geltendmachung, Ausübung oder Verteidigung
+          von Rechtsansprüchen oder zum Schutz der Rechte einer anderen
+          natürlichen oder juristischen Person oder aus Gründen eines wichtigen
+          öffentlichen Interesses der Europäischen Union oder eines
+          Mitgliedstaats verarbeitet werden.
+        </p>
+
+        <h4 className={styles["sub-headings"]}>
+          4. Datenerfassung auf dieser Website
+        </h4>
+
+        <h5 className={styles["secondary-sub-headings"]}>Kontaktformular</h5>
+
+        <p className={styles["paragraphs"]}>
+          Wenn Sie uns per Kontaktformular Anfragen zukommen lassen, werden Ihre
+          Angaben aus dem Anfrageformular inklusive der von Ihnen dort
+          angegebenen Kontaktdaten zwecks Bearbeitung der Anfrage und für den
+          Fall von Anschlussfragen bei uns gespeichert. Diese Daten geben wir
+          nicht ohne Ihre Einwilligung weiter.
+        </p>
+
+        <p className={styles["paragraphs"]}>
+          Die Verarbeitung dieser Daten erfolgt auf Grundlage von Art. 6 Abs. 1
+          lit. b DSGVO, sofern Ihre Anfrage mit der Erfüllung eines Vertrags
+          zusammenhängt oder zur Durchführung vorvertraglicher Maßnahmen
+          erforderlich ist. In allen übrigen Fällen beruht die Verarbeitung auf
+          unserem berechtigten Interesse an der effektiven Bearbeitung der an
+          uns gerichteten Anfragen (Art. 6 Abs. 1 lit. f DSGVO) oder auf Ihrer
+          Einwilligung (Art. 6 Abs. 1 lit. a DSGVO) sofern diese abgefragt
+          wurde; die Einwilligung ist jederzeit widerrufbar.
+        </p>
+
+        <p className={styles["paragraphs"]}>
+          Die von Ihnen im Kontaktformular eingegebenen Daten verbleiben bei
+          uns, bis Sie uns zur Löschung auffordern, Ihre Einwilligung zur
+          Speicherung widerrufen oder der Zweck für die Datenspeicherung
+          entfällt (z. B. nach abgeschlossener Bearbeitung Ihrer Anfrage).
+          Zwingende gesetzliche Bestimmungen – insbesondere Aufbewahrungsfristen
+          – bleiben unberührt.
+        </p>
+
+        <h5 className={styles["secondary-sub-headings"]}>
+          Anfrage per E-Mail, Telefon oder Telefax
+        </h5>
+
+        <p className={styles["paragraphs"]}>
+          Wenn Sie uns per E-Mail, Telefon oder Telefax kontaktieren, wird Ihre
+          Anfrage inklusive aller daraus hervorgehenden personenbezogenen Daten
+          (Name, Anfrage) zum Zwecke der Bearbeitung Ihres Anliegens bei uns
+          gespeichert und verarbeitet. Diese Daten geben wir nicht ohne Ihre
+          Einwilligung weiter.
+        </p>
+
+        <p className={styles["paragraphs"]}>
+          Die Verarbeitung dieser Daten erfolgt auf Grundlage von Art. 6 Abs. 1
+          lit. b DSGVO, sofern Ihre Anfrage mit der Erfüllung eines Vertrags
+          zusammenhängt oder zur Durchführung vorvertraglicher Maßnahmen
+          erforderlich ist. In allen übrigen Fällen beruht die Verarbeitung auf
+          unserem berechtigten Interesse an der effektiven Bearbeitung der an
+          uns gerichteten Anfragen (Art. 6 Abs. 1 lit. f DSGVO) oder auf Ihrer
+          Einwilligung (Art. 6 Abs. 1 lit. a DSGVO) sofern diese abgefragt
+          wurde; die Einwilligung ist jederzeit widerrufbar.
+        </p>
+
+        <p className={styles["paragraphs"]}>
+          Die von Ihnen an uns per Kontaktanfragen übersandten Daten verbleiben
+          bei uns, bis Sie uns zur Löschung auffordern, Ihre Einwilligung zur
+          Speicherung widerrufen oder der Zweck für die Datenspeicherung
+          entfällt (z. B. nach abgeschlossener Bearbeitung Ihres Anliegens).
+          Zwingende gesetzliche Bestimmungen – insbesondere gesetzliche
+          Aufbewahrungsfristen – bleiben unberührt.
+        </p>
+
+        <h4 className={styles["sub-headings"]}>5. Plugins und Tools</h4>
+
+        <h5 className={styles["secondary-sub-headings"]}>Google Maps</h5>
+
+        <p className={styles["paragraphs"]}>
+          Diese Seite nutzt den Kartendienst Google Maps. Anbieter ist die
+          Google Ireland Limited („Google“), Gordon House, Barrow Street, Dublin
+          4, Irland.
+        </p>
+
+        <p className={styles["paragraphs"]}>
+          Zur Nutzung der Funktionen von Google Maps ist es notwendig, Ihre
+          IP-Adresse zu speichern. Diese Informationen werden in der Regel an
+          einen Server von Google in den USA übertragen und dort gespeichert.
+          Der Anbieter dieser Seite hat keinen Einfluss auf diese
+          Datenübertragung. Wenn Google Maps aktiviert ist, kann Google zum
+          Zwecke der einheitlichen Darstellung der Schriftarten Google Web Fonts
+          verwenden. Beim Aufruf von Google Maps lädt Ihr Browser die benötigten
+          Web Fonts in ihren Browsercache, um Texte und Schriftarten korrekt
+          anzuzeigen.
+        </p>
+
+        <p className={styles["paragraphs"]}>
+          Die Nutzung von Google Maps erfolgt im Interesse einer ansprechenden
+          Darstellung unserer Online- Angebote und an einer leichten
+          Auffindbarkeit der von uns auf der Website angegebenen Orte. Dies
+          stellt ein berechtigtes Interesse im Sinne von Art. 6 Abs. 1 lit. f
+          DSGVO dar. Sofern eine entsprechende Einwilligung abgefragt wurde,
+          erfolgt die Verarbeitung ausschließlich auf Grundlage von Art. 6 Abs.
+          1 lit. a DSGVO und § 25 Abs. 1 TTDSG, soweit die Einwilligung die
+          Speicherung von Cookies oder den Zugriff auf Informationen im Endgerät
+          des Nutzers (z. B. Device-Fingerprinting) im Sinne des TTDSG umfasst.
+          Die Einwilligung ist jederzeit widerrufbar.
+        </p>
+
+        <span className={styles["spans"]}>
+          Die Datenübertragung in die USA wird auf die Standardvertragsklauseln
+          der EU-Kommission gestützt. Details finden Sie hier:
+          <a
+            className={styles["anchor-tags"]}
+            href="https://privacy.google.com/businesses/gdprcontrollerterms/ "
+            alt="Link to google privacy controller terms"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            https://privacy.google.com/businesses/gdprcontrollerterms/{" "}
+          </a>
+          und
+          <br />
+          <a
+            className={styles["anchor-tags"]}
+            href="https://privacy.google.com/businesses/gdprcontrollerterms/sccs/"
+            alt="Link to google privacy controller terms sccs"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            https://privacy.google.com/businesses/gdprcontrollerterms/sccs/{" "}
+          </a>
+          .
+        </span>
+
+        <span className={styles["spans"]}>
+          Mehr Informationen zum Umgang mit Nutzerdaten finden Sie in der
+          Datenschutzerklärung von Google:
+          <a
+            className={styles["anchor-tags"]}
+            href="https://policies.google.com/privacy?hl=de"
+            alt="Link to google privacy policy"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            https://policies.google.com/privacy?hl=de{" "}
+          </a>
+          .
+        </span>
+
+        <span className={styles["spans"]}>
+          Quelle:
+          <a
+            className={styles["anchor-tags"]}
+            href="https://www.e-recht24.de"
+            alt="Link to e-recht 24"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            https://www.e-recht24.de{" "}
+          </a>
+          .
+        </span>
       </div>
     </div>
   );
