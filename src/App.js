8eae56ba92052cd86e7bc34059b07c34b6feef61
Edith-Heidmann <eheidmann@gmail.com>
2022-06-07 13:18:37 +0200

    Tested z-index again

--- src/App.js
+++ src/App.js
@@ -85,7 +85,6 @@ const App = () => {
             <CookieConsent
               location="bottom"
               buttonText="Got it!"
-              z-index="200000"
               // cookieName="myAwesomeCookieName2"
               style={{ background: "#f2f2f2" }}
               buttonStyle={{
@@ -94,6 +93,7 @@ const App = () => {
                 background: "#1a6f5c",
                 padding: "10px 20px 10px 20px",
                 borderRadius: "5px",
+                zIndex: "2000",
               }}
               expires={150}
             >
