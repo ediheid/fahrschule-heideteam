e59b0c4ce1013dfc94de1e34d46a5f7deb823700
Edith-Heidmann <eheidmann@gmail.com>
2022-06-09 13:31:14 +0200

    Made Hashlinks smooth

--- src/Views/Klassen/SubMenu.js
+++ src/Views/Klassen/SubMenu.js
@@ -31,51 +31,55 @@ const SubMenu = () => {
     >
       {/* // Todo: Hashlinks ready to setup to sections once implemented */}
       <ul>
-        <HashLink to="/klassen#klasse-a" className={styles["hashlink"]}>
+        <HashLink smooth to="/klassen#klasse-a" className={styles["hashlink"]}>
           <li>A</li>
         </HashLink>
-        <HashLink to="/klassen#klasse-a1" className={styles["hashlink"]}>
+        <HashLink smooth to="/klassen#klasse-a1" className={styles["hashlink"]}>
           {" "}
           <li>A1</li>
         </HashLink>
-        <HashLink to="/klassen#klasse-a2" className={styles["hashlink"]}>
+        <HashLink smooth to="/klassen#klasse-a2" className={styles["hashlink"]}>
           <li>A2</li>
         </HashLink>
-        <HashLink to="/klassen#klasse-am" className={styles["hashlink"]}>
+        <HashLink smooth to="/klassen#klasse-am" className={styles["hashlink"]}>
           {" "}
           <li>AM</li>
         </HashLink>
-        <HashLink to="/klassen#klasse-b" className={styles["hashlink"]}>
+        <HashLink smooth to="/klassen#klasse-b" className={styles["hashlink"]}>
           {" "}
           <li>B / B197</li>
         </HashLink>
-        <HashLink to="/klassen#klasse-be" className={styles["hashlink"]}>
+        <HashLink smooth to="/klassen#klasse-be" className={styles["hashlink"]}>
           <li>BE</li>
         </HashLink>
-        <HashLink to="/klassen#klasse-c" className={styles["hashlink"]}>
+        <HashLink smooth to="/klassen#klasse-c" className={styles["hashlink"]}>
           <li>C</li>
         </HashLink>
-        <HashLink to="/klassen#klasse-c1" className={styles["hashlink"]}>
+        <HashLink smooth to="/klassen#klasse-c1" className={styles["hashlink"]}>
           {" "}
           <li>C1</li>
         </HashLink>
-        <HashLink to="/klassen#klasse-c1e" className={styles["hashlink"]}>
+        <HashLink
+          smooth
+          to="/klassen#klasse-c1e"
+          className={styles["hashlink"]}
+        >
           {" "}
           <li>C1E</li>
         </HashLink>
-        <HashLink to="/klassen#klasse-ce" className={styles["hashlink"]}>
+        <HashLink smooth to="/klassen#klasse-ce" className={styles["hashlink"]}>
           <li>CE</li>
         </HashLink>
-        <HashLink to="/klassen#klasse-d" className={styles["hashlink"]}>
+        <HashLink smooth to="/klassen#klasse-d" className={styles["hashlink"]}>
           <li>D</li>
         </HashLink>
-        <HashLink to="/klassen#klasse-d1" className={styles["hashlink"]}>
+        <HashLink smooth to="/klassen#klasse-d1" className={styles["hashlink"]}>
           <li>D1</li>
         </HashLink>
-        <HashLink to="/klassen#klasse-t" className={styles["hashlink"]}>
+        <HashLink smooth to="/klassen#klasse-t" className={styles["hashlink"]}>
           <li>T</li>
         </HashLink>
-        <HashLink to="/klassen#galerie" className={styles["hashlink"]}>
+        <HashLink smooth to="/klassen#galerie" className={styles["hashlink"]}>
           <li>Galerie</li>
         </HashLink>
       </ul>
